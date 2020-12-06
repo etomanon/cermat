a1975997f6b78b6c479a0cd14938d1db3ab1a84a
Jan Sikora <94.sikora.jan@gmail.com>
2020-12-06 18:55:15 +0100

    srid

--- server/src/utils/objection/objection-postgis.ts
+++ server/src/utils/objection/objection-postgis.ts
@@ -4,4 +4,5 @@ export const objectionPostgisPoint = (lng: number, lat: number) =>
   raw(`ST_GeomFromGeoJSON('{
   "type": "Point",
   "coordinates": [${lng}, ${lat}],
+  "crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 }')`)
