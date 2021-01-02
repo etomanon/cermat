cda87b18b8f650d5e61f026e40b0bad1055ad803
Jan Sikora <94.sikora.jan@gmail.com>
2021-01-02 16:16:29 +0100

    github actions

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -27,7 +27,7 @@ export const Home = () => {
               mb={['1rem', '1rem', 0]}
             >
               <Typography variant="h1" gutterBottom>
-                Zjistěte maturitní výsledky škol
+                Zjistěte maturitní výsledky škol v2
               </Typography>
               <SearchSchool />
             </Box>
